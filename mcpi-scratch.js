9947071729d6c3d3746226788413a9f882c1cd21
72martinp <martinpelikan72@gmail.com>
2017-10-27 11:52:18 +0200

    sdsdd

--- mcpi-scratch.js
+++ mcpi-scratch.js
@@ -234,7 +234,8 @@
             setCircle: "set circle center x1:%n z1:%n radius r:%n at height y:%n to type %n data %n",
             getPlayerPos:"get player pos %m.pos",
             getBlock:"get block pos x:%n y:%n z:%n %m.blockPos",
-            getHeight:"get height pos x:%n z:%n",           
+            getHeight:"get height pos x:%n z:%n",   
+            turtleForward:"move %n steps",			
             whenBlockHit: "when blockHit",
             message:"message"
         },
@@ -284,6 +285,7 @@
             [" ", translate.setBlocks,"setBlocks", 0, 0, 0, 0, 0, 0, 1, -1],
             [" ", translate.setLine,"setLine", 0, 0, 0, 0, 0, 1, -1],
             [" ", translate.setCircle,"setCircle", 0, 0, 0, 0, 0, 1, -1],
+			[" ", translate.turtleForward,"turtleForward", 0],
             ["R", translate.getPlayerPos,"getPlayerPos", 'x'],
             ["R", translate.getBlock,"getBlock", 0, 0, 0],
             ["R", translate.getHeight,"getHeight", 0, 0],
